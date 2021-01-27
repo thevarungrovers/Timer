2410067cda464bd3f408138b2d7252ed4df9b33d
Varun Grover <grover82vinni@gmail.com>
2021-01-27 21:34:18 +0530

    timer completed

---
+++ README.md
@@ -0,0 +1,7 @@
+                     TIMER
+------------------------------------------------------
+
+Layout: https://getbootstrap.com/
+Glassmorphism: https://glassmorphism.com/
+Linear Gradient: https://uigradients.com/#EveningSunshine
+Timer used: https://www.w3schools.com/
--- js/time.js
+++ js/time.js
@@ -1,19 +1,18 @@
 function myFunction() {
- 
     var d = new Date();
     var lastmonth =d.getMonth() + 1;
     var lastdate = d.getDate(); ;
     var lastyear = d.getFullYear(); ;
     var lasthr = document.getElementById('lasthr').value ;
     var lastmin = document.getElementById('lastmin').value ;
-    var lastsec = document.getElementById('lastsec').value ;
+    var lastsec = d.getSeconds(); ;
 
     var timeentered = lastmonth + ' ' + lastdate + ', ' + lastyear + ' ' + lasthr + ':' + lastmin + ':' + lastsec
 
     var countDownDate = new Date(timeentered).getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+    var x = setInterval(function() {
 
     // Get today's date and time
     var now = new Date().getTime();
@@ -35,4 +34,8 @@ var x = setInterval(function() {
       document.getElementById("demo").innerHTML = "TIME OVER!!";
     }
   }, 1000);
+}
+
+function showtext(){
+  document.getElementById('datatimer').style.display = 'block'
 }
